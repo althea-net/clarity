c74728f2ed8e20d2758e0a9d094eb3b068fd212f
Michał Papierski <michal@papierski.net>
2018-09-13 13:27:56 +0200

    Change approach a bit regarding tests collection.
    This adds ignored for tests that we don't support yet (the ones without
    transaction elements), and changes the approach to verification of valid
    transactions. When a test is expected to fail (with "invalid" result in
    a filler) it will be marked as "ShouldPanic".

--- tests/transaction_tests.rs
+++ tests/transaction_tests.rs
@@ -16,7 +16,7 @@ use std::fs::{self, DirEntry, File};
 use std::io;
 use std::io::BufReader;
 use std::path::{Path, PathBuf};
-use test::{DynTestFn, DynTestName, TestDesc, TestDescAndFn};
+use test::{DynTestFn, DynTestName, ShouldPanic, TestDesc, TestDescAndFn};
 
 fn visit_dirs(dir: &Path, cb: &mut FnMut(&DirEntry)) -> io::Result<()> {
     if dir.is_dir() {
@@ -46,26 +46,16 @@ fn default_gas_limit() -> String {
 }
 
 #[derive(Deserialize, Debug)]
-struct TestFillerTransaction {
-    data: String,
-    #[serde(rename = "gasLimit", default = "default_gas_limit")]
-    gas_limit: String,
-    #[serde(rename = "gasPrice")]
-    gas_price: String,
-    nonce: String,
-    to: String,
-    #[serde(default = "String::new")]
-    value: String,
-    v: String,
-    r: String,
-    s: String,
-}
-
-#[derive(Deserialize, Debug)]
 struct TestFiller {
     // I.e. [{"network": ["ALL"], "result": "invalid"}]
     expect: Option<Vec<TestFillerExpect>>,
-    transaction: Option<TestFillerTransaction>,
+    // This is kind of unnatural in our environment, but there is at least
+    // one test case where they have more transaction params than expected.
+    // It doesn't really matter in our case because we operate on structs,
+    // without any dynamic fields, but just to be sure, we can
+    // verify that this map has exactly 9 elements (or all expected
+    // elements exists).
+    transaction: Option<HashMap<String, String>>,
 }
 
 #[derive(Deserialize, Debug)]
@@ -133,67 +123,70 @@ fn make_test(path: PathBuf) -> Option<TestDescAndFn> {
     assert_eq!(filler.len(), 1);
     let (_name, filler) = filler.into_iter().nth(0).unwrap();
 
-    // As mentioned above skip invalid transactions
-    if filler.transaction.is_none() {
-        return None;
-    }
+    let mut desc = TestDesc::new(DynTestName(path.to_string_lossy().to_string()));
+
+    desc.should_panic = match filler.expect {
+        // Our tests should fail with a panic
+        Some(ref expect) if expect.get(0).as_ref().unwrap().result == "invalid" => ShouldPanic::Yes,
+        // All assertions shall pass
+        Some(ref expect) if expect.get(0).as_ref().unwrap().result == "valid" => ShouldPanic::No,
+        Some(_) => panic!("Invalid filler data {:?}", filler),
+        None => {
+            // No "expect" key means no "transaction" key
+            assert!(filler.transaction.is_none());
+            // Those tests are ignored anyway
+            ShouldPanic::YesWithMessage("This test is ignored")
+        }
+    };
+
+    // As mentioned above skip invalid transactions for now
+    desc.ignore = filler.transaction.is_none();
 
     Some(TestDescAndFn {
-        desc: TestDesc::new(DynTestName(path.to_string_lossy().to_string())),
+        desc: desc,
         testfn: DynTestFn(Box::new(move || {
             // TODO: The full test should go both ways i.e. construct TX based on filler values and compare with RLP, and another test should verify decoded RLP
             // and compare it.
-
-            assert!(fixtures.rlp.len() > 0usize);
-            // assert!(!filler.transaction.is_none());
-
             // We skipped all fillers without transaction data
             let raw_params = filler.transaction.as_ref().unwrap();
             let tx = Transaction {
-                nonce: raw_params.nonce.parse().unwrap_or(BigEndianInt::zero()),
-                gas_price: raw_params.gas_price.parse().unwrap_or(BigEndianInt::zero()),
-                gas_limit: raw_params
-                    .gas_limit
+                nonce: raw_params["nonce"].parse().unwrap_or(BigEndianInt::zero()),
+                gas_price: raw_params["gasPrice"]
+                    .parse()
+                    .unwrap_or(BigEndianInt::zero()),
+                gas_limit: raw_params["gasLimit"]
                     .parse()
                     .expect("Unable to parse gas_limit"),
-                to: raw_params.to.parse().unwrap_or(Address::default()),
-                value: raw_params.value.parse().unwrap_or(BigEndianInt::zero()),
-                data: hex_str_to_bytes(&raw_params.data).expect("Unable to parse data"),
+                to: raw_params["to"].parse().unwrap_or(Address::default()),
+                value: raw_params["value"].parse().unwrap_or(BigEndianInt::zero()),
+                data: hex_str_to_bytes(&raw_params["data"]).expect("Unable to parse data"),
                 signature: Some(Signature::new(
-                    raw_params.v.parse().expect("Unable to parse v"),
-                    raw_params.r.parse().expect("Unable to parse r"),
-                    raw_params.s.parse().expect("Unable to parse s"),
+                    raw_params["v"].parse().expect("Unable to parse v"),
+                    raw_params["r"].parse().expect("Unable to parse r"),
+                    raw_params["s"].parse().expect("Unable to parse s"),
                 )),
             };
 
             let our_rlp = to_bytes(&tx).unwrap();
 
             assert!(fixtures.rlp.starts_with("0x"));
-            match filler.expect {
-                Some(ref expect) if expect.get(0).as_ref().unwrap().result == "invalid" => {
-                    // assert_ne!(bytes_to_hex_str(&our_rlp), &fixtures.rlp[2..], "{:?} != {:?} (filler {:?})", &tx, &raw_params, &filler);
-                    assert!(!tx.is_valid(), "{:?} {:?} {:?}", tx, raw_params, filler);
-                    assert!(
-                        !tx.signature.as_ref().unwrap().is_valid(),
-                        "{:?} {:?} {:?}",
-                        tx.signature.as_ref().unwrap(),
-                        raw_params,
-                        filler
-                    );
-                }
-                Some(ref expect) if expect.get(0).as_ref().unwrap().result == "valid" => {
-                    assert_eq!(
-                        bytes_to_hex_str(&our_rlp),
-                        &fixtures.rlp[2..],
-                        "{:?} != {:?} (filler {:?})",
-                        &tx,
-                        &raw_params,
-                        &filler
-                    );
-                }
-                Some(_) => panic!("Invalid filler data {:?}!", filler),
-                None => unreachable!("This shouldn't happen"),
-            }
+
+            assert!(tx.is_valid(), "{:?} {:?} {:?}", tx, raw_params, filler);
+            assert!(
+                tx.signature.as_ref().unwrap().is_valid(),
+                "{:?} {:?} {:?}",
+                tx.signature.as_ref().unwrap(),
+                raw_params,
+                filler
+            );
+            assert_eq!(
+                bytes_to_hex_str(&our_rlp),
+                &fixtures.rlp[2..],
+                "{:?} != {:?} (filler {:?})",
+                &tx,
+                &raw_params,
+                &filler
+            );
         })),
     })
 }
